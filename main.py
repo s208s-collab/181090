c8349ff43c893fbbda03f7acd5bca389829fe1f5
s208s-collab <s208s@yandex.ru>
2026-08-19 00:36:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+"""Локальный запуск: python main.py"""
+
+import os
+
+import uvicorn
+
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="0.0.0.0", port=int(os.getenv("PORT", "8000")))
+
